140bfdc29defd3c75f139d44e45e2bc0d364d9e7
lzc978 <lzc978@126.com>
2019-10-31 16:04:58 +0800

    重写socketserver类并集成到项目中

--- dg_run.py
+++ dg_run.py
@@ -1,13 +1,16 @@
 import logging
 import os
 import time
+import socketserver
+from threading import Thread
 
 from digraph import Digraph, get_obj, save_obj, get_participle
 from inotify import main
+from securesocket import Handler
+from get_config import ReadConfigFile
 from utils import logger, single_instance
 from mysql_client import MysqlClient
 from word_method import Words
-# import utils.logger as logger
 
 
 mysql = MysqlClient(host='192.168.20.29',
@@ -77,71 +80,38 @@ class DGModel(object):
         return dg
 
 
+class TCPHandler(socketserver.BaseRequestHandler):
+    """重写handle"""
+
+    def setup(self):
+        pass
+
+    def handle(self):
+        print(self.client_address)
+        Handler(self.request)(self.server.dg)
+        # print(requests.q)  # q
+
+    def finish(self):
+        pass
+
+
+class ThreadingTCPServerExtra(socketserver.ThreadingTCPServer):
+    """重写tcp server类初始化状态"""
+
+    def __init__(self, cfg, server_address, RequestHandlerClass):
+        logging.basicConfig(level="DEBUG")
+
+        dgl = DGModel(cfg['digraph']['_sentence_path'])
+        self.dg = dgl(cfg['digraph']['_data_path']) if callable(dgl) else object
+        time.sleep(5)
+
+        Thread(target=main, args=(dgl, cfg)).start()
+
+        super().__init__(server_address, RequestHandlerClass)
+
+
 if __name__ == '__main__':
-    _data_path = "./model/dg.pkl"
-    _sentence_path = "./correct/correct.txt"
-    logging.basicConfig(level="DEBUG")
+    cfg = ReadConfigFile('config.ini').read_ini()
 
-    dgl = DGModel(_sentence_path)
-    dg = dgl(_data_path) if callable(dgl) else object
-    import time
-    time.sleep(5)
-
-    from threading import Thread
-    Thread(target=main, args=(dgl, )).start()
-
-    # file_name = "./model/dg.pkl"
-    # print(os.path.isfile(file_name))
-    # if os.path.isfile(file_name):
-    #     begin1 = time.time()
-    #     dg = get_obj(file_name=file_name)
-    #     logger.debug("读取耗时:{}秒".format(time.time() - begin1))
-    # else:
-    #     sentences = ["""我 想 去 北京""",
-    #                  """我 想 吃饭 了""",
-    #                  """北京 在 哪里""",
-    #                  """我 想要 办理 汉口 银行 的 信用卡""",
-    #                  """怎么 办理 信用卡""",
-    #                  """信用卡 怎么 办理""",
-    #                  """我 要 取号""",
-    #                  """我 想去 天安门""",]
-    #     # for i in [['我', '要', '取号'], ['我', '要', '吃饭']]:
-    #     with open('correct.txt', 'r', encoding='utf-8') as f:
-    #         for content in f.readlines():
-    #             sentences_list = get_participle(content)
-    #             sentences.extend(w.gen_sentences(sentences_list, subsystem_id='hm256f8c953411e8aea35254004210bf'))
-    #     # sentences = [w.gen_sentences(i, subsystem_id='hm256f8c953411e8aea35254004210bf') for i in [['我', '要', '取号'], ['我', '要', '吃饭']]]
-    #     # sentences.extend(sentences)
-    #
-    #     word_list = [item.split() for item in sentences]
-    #     # temp = [set(item) for item in word_list]
-    #     # word_set = reduce(lambda a, b: a | b, temp)  # 保证词语不重复
-    #     # logger.debug(word_set)
-    #     begin1 = time.time()
-    #     # dg = Digraph(word_set)
-    #     # # 构建词序
-    #     # for words in word_list:
-    #     #     l = len(words)
-    #     #     for i in range(l):
-    #     #         if i < l - 1:
-    #     #             dg.addEdge(words[i], words[i + 1])
-    #     dg = Digraph()
-    #     for w_l in word_list:
-    #         dg.build(w_l)
-    #     logger.debug("构建耗时:{}秒".format(time.time() - begin1))
-    #     save_obj(dg, file_name=file_name)
-    # dg.print()
-
-    logger.debug("*" * 10 + "测试" + "*" * 10)
-
-    begin3 = time.time()
-    # 我 要 取号
-    logger.debug(dg.search("我 线 池法".split()))
-    logger.debug("查询2耗时{}秒\n".format(time.time() - begin3))
-
-    sw = w.get_synonym_words()
-    print(sw)
-
-    res = w.gen_sentences(['我', '能', '取号', '吗'], subsystem_id='hm256f8c953411e8aea35254004210bf')
-
-    print(res)
+    server = ThreadingTCPServerExtra(cfg, (cfg['tcpsocket']['host'], cfg['tcpsocket']['port']), TCPHandler)
+    server.serve_forever()
---
+++ securesocket.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# async socket 编解码读取写入并传输
+
+__author__ = 'BraveHeart'
+__version__ = 0.1
+
+import pickle
+
+from digraph import printf
+from utils import logger
+
+
+class Handler(object):
+
+    def __init__(self, request):
+        self.requests = request
+
+    def _send(self, data):
+        self.requests.sendall(pickle.dumps(data))
+
+    def _receive(self):
+        try: data = self.requests.recv(1024)
+        except EOFError as err: logger.error(f"客户端断开链接：{err}"); return False
+        return pickle.loads(data)
+
+    def __call__(self, dg):
+        content = \
+            """{}请输入选项{}\n0. exit | 1. print | 2. input\n{}""".format('*' * 10, '*' * 10, '*' * 29)
+        self._send(content)
+        _flag = 1
+        while _flag:
+            data = self._receive()
+            if not data:
+                break
+            logger.info(f"接收到需要纠错的句子：{data}\n\t有向图类型{type(dg)}")
+            if all(map(lambda c: '\u4e00' <= c <= '\u9fa5', data)):
+                corr_res = printf(dg, data)
+            else: logger.error(f"请输入中文：{data}"); corr_res = f"请输入中文：{data}"
+            self._send(corr_res)
+
+    def connect(self):
+        pass
+
+    def send_message(self):
+        pass
+
+
+if __name__ == '__main__':
+    # Handler()
+    ...
